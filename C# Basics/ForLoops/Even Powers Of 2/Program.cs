52826cba4aa0118e8341b785d5e9f20b740656ef
Plmn42 <plam_02@abv.bg>
2023-07-23 17:55:06 +0300

    update

---
+++ C# Basics/ForLoops/Even Powers Of 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Even_Powers_Of_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            for (int i = 0; i < input.Length; i++) 
+            {
+
+            }
+        }
+    }
+}
